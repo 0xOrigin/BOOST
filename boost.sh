469e95f36824eece5a676d1b0d23488400beaec0
0xOrigin <41273735+0xOrigin@users.noreply.github.com>
2018-07-22 02:52:52 +0200

    Shell Tool

--- boost.sh
+++ boost.sh
@@ -1,4 +1,25 @@
-echo " 
+####################################################################### Variables ###################################################################################
+first=$1
+second=$2
+third=$3
+pro=1
+date1=`  date   +%s`
+## Colors ##
+lightred="\e[91m"
+lightcyan="\e[96m"
+lightgreen="\e[92m"
+lightblue="\e[94m"
+lightyellow="\e[93m"
+lightmagenta="\e[95m"
+lightblue="\e[94m"
+blue="\e[34m"
+endcolor="\e[0m"
+backblue="\e[44m"
+bold="\e[1m"
+color="\e[38;5;208m"
+color2="\e[38;5;228m"
+######################################################################## Beginning ###################################################################################
+echo -e "$lightred 
                          ____   ____   ____   _____ _______ 
                         |  _ \ / __ \ / __ \ / ____|__   __|
                         | |_) | |  | | |  | | (___    | |   
@@ -6,39 +27,38 @@ echo "
                         | |_) | |__| | |__| |____) |  | |   
                         |____/ \____/ \____/|_____/   |_|   
                                                                        
-                                 By - @0xOrigin                               
+                                $endcolor By - @0xOrigin                               
 "
-echo "              "
-####################################################################### Variables ###################################################################################
-first=$1
-second=$2
-third=$3
-pro=1
-date1=`  date   +%s`
+echo -e "              "
 #################################################################### Check First Parameter ##########################################################################
 if [ $first ]
 then
     if [ $first = "-h" -o $first = "--help" ]
     then
-        echo "Usage: boost.sh [OPTIONS...] <list_name> <output_name>"            
-        echo "       boost.sh [-h] Help, [-l] Domains list, [-c] Content spoofing."
-        echo "              "
-        echo "OPTIONS:"
-        echo "  -h, --help              Show this help message and Exit."
-        echo "  -l, --list              Domain list to Check the status of the site and put domains in HTML code."
-        echo "  -c, --contentspoof      Check the domains that contain Content spoofing."
+        echo -e "Usage: boost.sh [OPTIONS...] <list_name> <output_name>"            
+        echo -e "       boost.sh [-h] Help, [-l] Domains list, [-c] Content spoofing."
+        echo -e "              "
+        echo -e "OPTIONS:"
+        echo -e "  -h, --help              Show this help message and Exit."
+        echo -e "  -l, --list              Domain list to Check the status of the site and put domains in HTML code."
+        echo -e "  -c, --contentspoof      Check the domains that contain Content spoofing."
         exit
     elif [ $first != "-h" -a $first != "--help" -a $first != "-l" -a $first != "--list" -a $first != "-c" -a $first != "--contentspoof" ]
     then
-        echo "[-] Try './boost.sh --help' for more information."
+        echo -e "$lightmagenta[-]$lightgreen The Method Not Vaild $lightmagenta,$lightgreen Try$lightcyan '$lightred./boost.sh --help$lightcyan'$lightgreen for more information$lightmagenta.$endcolor"
         exit
     fi
 else
-    echo "[+] Please Enter Method:"
+    echo -e "$lightmagenta[+]$lightgreen Please Enter Method$lightmagenta:$endcolor  \c"
     read first
+    while [ -z $first ]
+    do
+    echo -e echo -e "$lightmagenta[-]$lightgreen The Method Not Vaild $lightmagenta,$lightgreen Try$lightcyan '$lightred./boost.sh --help$lightcyan'$lightgreen for more information$lightmagenta.$endcolor"
+    exit
+    done
     if [ $first != "-h" -a $first != "--help" -a $first != "-l" -a $first != "--list" -a $first != "-c" -a $first != "--contentspoof" ]
     then
-        echo "[-] Try './boost.sh --help' for more information."
+        echo -e echo -e "$lightmagenta[-]$lightgreen The Method Not Vaild $lightmagenta,$lightgreen Try$lightcyan '$lightred./boost.sh --help$lightcyan'$lightgreen for more information$lightmagenta.$endcolor"
         exit
     fi
 fi
@@ -50,45 +70,61 @@ then
         ws=`cat $second`
         nm=`wc -l $second | cut -d"." -f1 | cut -d" " -f1`
     else
-        echo "[-] The File Not Found."
+        echo -e "$lightmagenta[-]$lightgreen The File Not Found$lightmagenta.$endcolor"
         exit
     fi
 else
-    echo "[+] Please Enter Websites List:"
+    echo -e "$lightmagenta[+]$lightgreen Please Enter Websites List$lightmagenta:$endcolor  \c"
     read second
+    echo -e "              "
+    while [ -z $second ]
+    do
+    echo -e "$lightmagenta[-]$lightgreen The File Not Found$lightmagenta.$endcolor"
+    exit
+    done
     if [ -e $second  ]
     then
         ws=`cat $second`
         nm=`wc -l $second | cut -d"." -f1 | cut -d" " -f1`
     else
-        echo "[-] The File Not Found."
+        echo -e "$lightmagenta[-]$lightgreen The File Not Found$lightmagenta.$endcolor"
         exit
     fi
 fi
-echo "              "
 #################################################################### Check Third Parameter ########################################################################## 
 if [ $third ]
 then
     if [ $first = "-l" -o $first = "--list" ]
         then
-            echo "[-] Will be saved in : `pwd`/Output/List/html/$third.$$.html "
-            echo "[-] Will be saved in : `pwd`/Output/List/txt/$third.$$.txt "
+            echo -e "$lightmagenta[-]$lightgreen Will be saved in $lightmagenta: $lightyellow`pwd`/Output/List/html/$third.$$.html$endcolor"
+            echo -e "$lightmagenta[-]$lightgreen Will be saved in $lightmagenta: $lightyellow`pwd`/Output/List/txt/$third.$$.txt$endcolor"
     elif [ $first = "-c" -o $first = "--contentspoof" ]
         then
-            echo "[-] Will be saved in : `pwd`/Output/Contentspoofing/html/$third.$$.html "
-            echo "[-] Will be saved in : `pwd`/Output/Contentspoofing/txt/$third.$$.txt "      
-        fi        
+            echo -e "$lightmagenta[-]$lightgreen Will be saved in $lightmagenta: $lightyellow`pwd`/Output/Contentspoofing/html/$third.$$.html$endcolor"
+            echo -e "$lightmagenta[-]$lightgreen Will be saved in $lightmagenta: $lightyellow`pwd`/Output/Contentspoofing/txt/$third.$$.txt$endcolor"      
+        fi    
 else
-    echo "[+] Please Enter Output Name:"
+    echo -e "$lightmagenta[+]$lightgreen Please Enter Output Name$lightmagenta:$endcolor  \c"
     read third
+    echo -e "              "
+    while [ -z $third ]
+    do
+    echo -e "$lightmagenta[-]$lightgreen The Output Name Not Valid$lightmagenta.$endcolor"
+    exit
+    done
+    while [ $third = " " ]
+    do
+    echo -e "$lightmagenta[-]$lightgreen The Output Name Not Valid$lightmagenta.$endcolor"
+    exit
+    done
             if [ $first = "-l" -o $first = "--list" ]
                 then
-                    echo "[-] Will be saved in : `pwd`/Output/List/html/$third.$$.html "
-                    echo "[-] Will be saved in : `pwd`/Output/List/txt/$third.$$.txt "
+                    echo -e "$lightmagenta[-]$lightgreen Will be saved in $lightmagenta: $lightyellow`pwd`/Output/List/html/$third.$$.html$endcolor"
+                    echo -e "$lightmagenta[-]$lightgreen Will be saved in $lightmagenta: $lightyellow`pwd`/Output/List/txt/$third.$$.txt$endcolor"
             elif [ $first = "-c" -o $first = "--contentspoof" ]
                 then
-                    echo "[-] Will be saved in : `pwd`/Output/Contentspoofing/html/$third.$$.html "
-                    echo "[-] Will be saved in : `pwd`/Output/Contentspoofing/txt/$third.$$.txt "
+                    echo -e "$lightmagenta[-]$lightgreen Will be saved in $lightmagenta: $lightyellow`pwd`/Output/Contentspoofing/html/$third.$$.html$endcolor"
+                    echo -e "$lightmagenta[-]$lightgreen Will be saved in $lightmagenta: $lightyellow`pwd`/Output/Contentspoofing/txt/$third.$$.txt$endcolor"
                 fi   
 fi
 ######################################################################### Check Files ################################################################################
@@ -134,115 +170,115 @@ else
     cd ..
 fi
 ####################################################################### Main Function ################################################################################
-echo "              "
+echo -e "              "
 if [ $first = "-l" -o $first = "--list" ]
 then
-    echo "#--------------Starting BOOST List TOOL at `date +"%Y-%m-%d %T %Z"`-------------#"
-    echo "              "
-    echo "[-] WebSites:"$nm
-    echo "      "
-    echo "<html>" > List/html/$third.$$.html
-    echo "<body>" >> List/html/$third.$$.html
-    echo "<h1 align="center">BOOST List TOOL</h1>" >> List/html/$third.$$.html
-    echo "<ol>" >> List/html/$third.$$.html
+    echo -e "$bold$blue#$endcolor--------------"$color"Starting BOOST List TOOL at $lightcyan`date +"%Y-%m-%d %T$color %Z"`$endcolor-------------$bold$blue#$endcolor"
+    echo -e "              "
+    echo -e "$lightmagenta[-]$lightgreen WebSites$lightmagenta:$endcolor"$nm
+    echo -e "      "
+    echo -e "<html>" > List/html/$third.$$.html
+    echo -e "<body>" >> List/html/$third.$$.html
+    echo -e "<h1 align="center">BOOST List TOOL</h1>" >> List/html/$third.$$.html
+    echo -e "<ol>" >> List/html/$third.$$.html
 while true 
 do
     while [ $pro -le $nm ]
     do
         for load in $ws
         do
-          if echo "Y" | GET -s -d -t 3 $load | grep "certificate verify failed" > /dev/null
+          if echo -e "Y" | GET -s -d -t 3 $load | grep "certificate verify failed" > /dev/null
           then
-              echo "<li><a href="http://$load" target="_blank">$load</a></li>" >> List/html/$third.$$.html
-              echo $load >> List/txt/$third.$$.txt
-          elif echo "Y" | GET -s -d -t 3 $load | grep "hostname verification failed" > /dev/null
+              echo -e "<li><a href="http://$load" target="_blank">$load</a></li>" >> List/html/$third.$$.html
+              echo -e $load >> List/txt/$third.$$.txt
+          elif echo -e "Y" | GET -s -d -t 3 $load | grep "hostname verification failed" > /dev/null
           then
-              echo "<li><a href="http://$load" target="_blank">$load</a></li>" >> List/html/$third.$$.html
-              echo $load >> List/txt/$third.$$.txt
+              echo -e "<li><a href="http://$load" target="_blank">$load</a></li>" >> List/html/$third.$$.html
+              echo -e $load >> List/txt/$third.$$.txt
           fi
 
-            if ! echo "Y" | GET -s -d -t 5 $load | grep "500" > /dev/null
+            if ! echo -e "Y" | GET -s -d -t 5 $load | grep "500" > /dev/null
             then 
-
-                echo "<li><a href="http://$load" target="_blank">$load</a></li>" >> List/html/$third.$$.html
-                echo $load >> List/txt/$third.$$.txt
-                echo -ne "                                                                    $(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)\r";
-                echo "$pro- Processing....."  
+                echo -e "<li><a href="http://$load" target="_blank">$load</a></li>" >> List/html/$third.$$.html
+                echo -e $load >> List/txt/$third.$$.txt
+                ex=`expr $nm - $pro`
+                echo -ne "\\r$lightcyan$pro$lightmagenta-$lightgreen Processing$lightmagenta.....$lightgreen Remaining $endcolor$ex                                     $lightcyan$(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)$endcolor  "\
+                
+                
             fi
             let pro=pro+1 
-        done       
+        done  
     done
-echo "                  "
-echo "[-] It takes $(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S) seconds to Complete this Task." 
+echo -e "                  \n"
+echo -e "$lightmagenta[-]$lightgreen It takes $lightcyan$(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)$lightgreen seconds to Complete this Task$lightmagenta.$endcolor" 
 break
 done  
- 
-        echo "</ol>" >> List/html/$third.$$.html
-        echo "</body>" >> List/html/$third.$$.html
-        echo "</html>" >> List/html/$third.$$.html
+        echo -e "</ol>" >> List/html/$third.$$.html
+        echo -e "</body>" >> List/html/$third.$$.html
+        echo -e "</html>" >> List/html/$third.$$.html
 
         sleep 1
-        echo "          "
-
-        echo "#------------------------Done at `date +"%Y-%m-%d %T %Z"`-----------------------#"
-        echo "      "
-                    echo "[-] Saved in : `pwd`/Output/List/html/$third.$$.html "
-                    echo "[-] Saved in : `pwd`/Output/List/txt/$third.$$.txt "     
-        echo "          "       
+        echo -e "          "
+        echo -e "$bold$blue#$endcolor------------------------"$color"Done at $lightcyan`date +"%Y-%m-%d %T$color %Z"`$endcolor-----------------------$bold$blue#$endcolor"
+        echo -e "      "
+                    echo -e "$lightmagenta[-]$lightgreen Saved in $lightmagenta: $lightyellow`pwd`/Output/List/html/$third.$$.html$endcolor"
+                    echo -e "$lightmagenta[-]$lightgreen Saved in $lightmagenta: $lightyellow`pwd`/Output/List/txt/$third.$$.txt$endcolor"     
+        echo -e "          "       
         xdg-open "List/html/$third.$$.html"
 
 elif [ $first = "-c" -o $first = "--contentspoof" ]
 then
-    echo "#--------Starting BOOST Content spoofing TOOL at `date +"%Y-%m-%d %T %Z"`-------#"
-    echo "              "
-    echo "[-] WebSites:"$nm
-    echo "      "
-    echo "<html>" > Contentspoofing/html/$third.$$.html
-    echo "<body>" >> Contentspoofing/html/$third.$$.html
-    echo "<h1 align="center">BOOST Content spoofing TOOL</h1>" >> Contentspoofing/html/$third.$$.html
-    echo "<ol>" >> Contentspoofing/html/$third.$$.html
+    echo -e "$bold$blue#$endcolor--------"$color"Starting BOOST Content spoofing TOOL at $lightcyan`date +"%Y-%m-%d %T$color %Z"`$endcolor-------$bold$blue#$endcolor"
+    echo -e "              "
+    echo -e "$lightmagenta[-]$lightgreen WebSites$lightmagenta:$endcolor"$nm
+    echo -e "      "
+    echo -e "<html>" > Contentspoofing/html/$third.$$.html
+    echo -e "<body>" >> Contentspoofing/html/$third.$$.html
+    echo -e "<h1 align="center">BOOST Content spoofing TOOL</h1>" >> Contentspoofing/html/$third.$$.html
+    echo -e "<ol>" >> Contentspoofing/html/$third.$$.html
 while true 
 do
 while [ $pro -le $nm ]
 do
   for load in $ws
   do
-            if echo "Y" | GET -s -d -t 2 $load | grep "certificate verify failed" > /dev/null
+            if echo -e "Y" | GET -s -d -t 2 $load | grep "certificate verify failed" > /dev/null
               then
-                  echo "<li><a href="http://$load" target="_blank">$load</a></li>" >> Contentspoofing/html/$third.$$.html
-                  echo $load >> Contentspoofing/txt/$third.$$.txt
-            elif echo "Y" | GET -s -d -t 3 $load | grep "hostname verification failed" > /dev/null
+                  echo -e "<li><a href="http://$load" target="_blank">$load</a></li>" >> Contentspoofing/html/$third.$$.html
+                  echo -e $load >> Contentspoofing/txt/$third.$$.txt
+            elif echo -e "Y" | GET -s -d -t 3 $load | grep "hostname verification failed" > /dev/null
                 then
-                    echo "<li><a href="http://$load" target="_blank">$load</a></li>" >> Contentspoofing/html/$third.$$.html
-                    echo $load >> Contentspoofing/txt/$third.$$.txt
+                    echo -e "<li><a href="http://$load" target="_blank">$load</a></li>" >> Contentspoofing/html/$third.$$.html
+                    echo -e $load >> Contentspoofing/txt/$third.$$.txt
               fi
-                if ! echo "Y" | GET -s -d -t 5 $load | grep "500" > /dev/null
+                if ! echo -e "Y" | GET -s -d -t 5 $load | grep "500" > /dev/null
                 then 
-        if echo "Y" | GET $load/It%20has%20been%20changed%20by%20a%20new%20one%20https://www.attacker.com%20so%20go%20to%20the%20new%20one%20since%20this%20one. | grep "changed" >> /dev/null
+        if echo -e "Y" | GET $load/It%20has%20been%20changed%20by%20a%20new%20one%20https://www.attacker.com%20so%20go%20to%20the%20new%20one%20since%20this%20one. | grep "changed" >> /dev/null
                 then
-                echo "<li><a href="http://$load/It%20has%20been%20changed%20by%20a%20new%20one%20https://www.attacker.com%20so%20go%20to%20the%20new%20one%20since%20this%20one." target="_blank">$load</a></li>" >> Contentspoofing/html/$third.$$.html
-                echo $load >> Contentspoofing/txt/$third.$$.txt
-                echo -ne "                                                                    $(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)\r";
-                echo "$pro- Processing....."
-        fi
-        fi
-        let pro=pro+1
+                echo -e "<li><a href="http://$load/It%20has%20been%20changed%20by%20a%20new%20one%20https://www.attacker.com%20so%20go%20to%20the%20new%20one%20since%20this%20one." target="_blank">$load</a></li>" >> Contentspoofing/html/$third.$$.html
+                echo -e $load >> Contentspoofing/txt/$third.$$.txt
+                ex=`expr $nm - $pro`
+                echo -ne "\\r$lightcyan$pro$lightmagenta-$lightgreen Processing$lightmagenta.....$lightgreen Remaining $endcolor$ex                                     $lightcyan$(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)$endcolor  "\
+
+            fi
+            fi
+            let pro=pro+1 
+done  
+echo -e "                  "
 done
-done
-        echo "                  "
-        echo "[-] It takes $(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S) seconds to Complete this Task." 
+        echo -e "$lightmagenta[-]$lightgreen It takes $lightcyan$(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)$lightgreen seconds to Complete this Task$lightmagenta.$endcolor"
         break
 done
-        echo "</ol>" >> Contentspoofing/html/$third.$$.html
-        echo "</body>" >> Contentspoofing/html/$third.$$.html
-        echo "</html>" >> Contentspoofing/html/$third.$$.html    
+        echo -e "</ol>" >> Contentspoofing/html/$third.$$.html
+        echo -e "</body>" >> Contentspoofing/html/$third.$$.html
+        echo -e "</html>" >> Contentspoofing/html/$third.$$.html    
         sleep 1
-        echo "          "
-        echo "#------------------------Done at `date +"%Y-%m-%d %T %Z"`-----------------------#"
-        echo "  "
-                    echo "[-] Saved in : `pwd`/Output/Contentspoofing/html/$third.$$.html "
-                    echo "[-] Saved in : `pwd`/Output/Contentspoofing/txt/$third.$$.txt " 
-        echo "          "
+        echo -e "          "
+        echo -e "$bold$blue#$endcolor------------------------"$color"Done at $lightcyan`date +"%Y-%m-%d %T$color %Z"`$endcolor-----------------------$bold$blue#$endcolor"
+        echo -e "  "
+                    echo -e "$lightmagenta[-]$lightgreen Saved in $lightmagenta: $lightyellow`pwd`/Output/Contentspoofing/html/$third.$$.html$endcolor"
+                    echo -e "$lightmagenta[-]$lightgreen Saved in $lightmagenta: $lightyellow`pwd`/Output/Contentspoofing/txt/$third.$$.txt$endcolor" 
+        echo -e "          "
         xdg-open "Contentspoofing/html/$third.$$.html"
 fi    
 ######################################################################### Main Function End ##########################################################################
